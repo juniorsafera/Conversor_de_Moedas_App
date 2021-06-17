c150f38dd91e5db17131fd41f9f2d60376a3f15d
Junior Safera <juniorsafera.dev@gmail.com>
2021-06-16 22:15:32 -0300

    Iniciando projeto

---
+++ conversor_de_moedas_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:conversor_de_moedas_app/views/ViewHome.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp( 
+    MaterialApp(
+
+      debugShowCheckedModeBanner: false ,
+      theme: ThemeData.dark(),
+      home: ViewHome()  ,
+      
+    )
+   );
+}
+
+ 
+
---
+++ conversor_de_moedas_app/lib/views/ViewHome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ViewHome extends StatefulWidget {
+  const ViewHome({ Key? key }) : super(key: key);
+
+  @override
+  _ViewHomeState createState() => _ViewHomeState();
+}
+
+class _ViewHomeState extends State<ViewHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
