fedc107a3cd06545e536161ba670f153a84a513a
Junior Safera <juniorsafera.dev@gmail.com>
2021-06-17 22:59:58 -0300

    View

--- conversor_de_moedas_app/lib/views/ViewHome.dart
+++ conversor_de_moedas_app/lib/views/ViewHome.dart
@@ -1,3 +1,5 @@
+ 
+
 import 'package:flutter/material.dart';
 
 class ViewHome extends StatefulWidget {
@@ -10,7 +12,62 @@ class ViewHome extends StatefulWidget {
 class _ViewHomeState extends State<ViewHome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body:   Container(
+          width: MediaQuery.of(context).size.width,
+          height:MediaQuery.of(context).size.height,
+          child: 
+          Padding(
+            padding: const EdgeInsets.only(top: 120  , left: 20 , right: 20, bottom: 20),
+            child: Column(
+               
+              children: [
+                 Container(
+                   width: 225,
+                   child:
+                    Image.network("https://image.flaticon.com/icons/png/512/356/356806.png"),
+                 ),
+
+                  Row(
+                    children: [
+
+                      Expanded(child:
+                       DropdownButton( items: [
+                        DropdownMenuItem(child: Text("Real"),),
+                        
+                      ],
+                       onTap: (){},
+                      )
+                      
+                      ),
+
+                      Expanded(
+                          child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder() ,
+                            labelText: "Valor" ,
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+      
+                // Widget de espaçamentos
+                 SizedBox(
+                   height: 50,
+                 ),
+
+
+                  RaisedButton(
+                    onPressed: (){},
+                    child: Text("CONVERTER"),
+                  ),
+
+              ],
+            ),
+          ),
+        ),
       
     );
   }
