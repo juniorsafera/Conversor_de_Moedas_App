6b62c100b906d1d76019f28e3e7d0a31a939089e
Junior Safera <juniorsafera.dev@gmail.com>
2021-06-22 17:52:56 -0300

    Criando o controller

---
+++ conversor_de_moedas_app/lib/controllers/home_controller.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:conversor_de_moedas_app/models/moedas_Model.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomeController{
+
+List<MoedasModel> moedas;
+TextEditingController toText = TextEditingController();
+TextEditingController fromText = TextEditingController();
+
+MoedasModel toMoeda;
+MoedasModel fromMoeda;
+
+HomeController() {
+  moedas = MoedasModel.getMoedas();
+  toMoeda = moedas[0];
+  fromMoeda = moedas[1];
+}
+
+
+void convert(){
+  String text = toText.text;
+  double value = double.tryParse(text) ?? 1.0;
+  double returnValue = 0;
+
+  if(fromMoeda.nome == 'Real'){
+    returnValue = value * toMoeda.real;
+  } else if(fromMoeda.nome == 'Dolar'){
+    returnValue = value * toMoeda.dolar;
+  } else if(fromMoeda.nome == 'Euro'){
+    returnValue = value * toMoeda.euro;
+  } else if(fromMoeda.nome == 'Bitcoin'){
+    returnValue = value * toMoeda.bitcoin;
+  }
+
+  fromText.text = returnValue.toStringAsFixed(2);
+}
+}
